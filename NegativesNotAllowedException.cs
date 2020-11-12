ac1fbc43b3883ef1061700e83fa6b2523e4a893b
Surya Avasarala <savasarala@cardinalcommerce.onmicrosoft.com>
2020-11-12 16:21:29 -0500

    second committ

---
+++ NegativesNotAllowedException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace StringCalculator
+{
+    public class NegativesNotAllowedException  :Exception
+    {
+        public NegativesNotAllowedException(IEnumerable<int> negativeNumbers) 
+            : base($"Negatives not allowed: {string.Join(",",negativeNumbers)}")
+        {
+        }
+
+    }
+}
---
+++ StringCalculator.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StringCalculator
+{
+    public class Calculator
+    {
+        public static int Add(string numbers)
+        {
+            string[] delimiters = GetDelimiters(numbers);
+            string[] strings = numbers.Split(delimiters, StringSplitOptions.None);
+            List<int> numberList = new List<int>();
+            foreach (var n in strings)
+            {
+                if (int.TryParse(n, out int number))
+                {
+
+                    numberList.Add(number);
+                }
+            }
+            CheckforNegativeNumbers(numberList);
+            numberList.RemoveAll(x => x > 1000);
+            return numberList.Sum();
+        }
+
+        private static void CheckforNegativeNumbers(List<int> numberList)
+        {
+            var negativeNumbers = numberList.Where(x => x < 0).ToList();
+            if (numberList.Any(x => x < 0))
+            {
+                throw new NegativesNotAllowedException(negativeNumbers);
+            }
+        }
+        private static string[] GetDelimiters(string numbers)
+        {
+            var delimiters = new List<string> { ",", "\n" };
+            if(numbers.StartsWith("//"))
+            {
+                string customDelim = numbers.Split('\n').First().Substring(2);
+                if (customDelim.StartsWith("["))
+                {
+                    delimiters.Add(customDelim.Substring(1, customDelim.Length-2));
+                }
+                else
+                {
+                    delimiters.Add(customDelim);
+                }
+            }
+            return delimiters.ToArray();
+        }
+    }
+}
